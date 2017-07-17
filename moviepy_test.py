1d39615d462d3650ad4948165074aa246eae1559
Markus Neumann <markus.neumann@uzh.ch>
2017-07-17 15:51:15 +0200

    added .gitignore fixed pep 8 violations

--- moviepy_test.py
+++ moviepy_test.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-import numpy as np
 from moviepy.editor import *
 from gtts import gTTS
 from aeneas.tools.execute_task import ExecuteTaskCLI
-from moviepy.video.tools.segmenting import findObjects
-import os, json
+import os
+import json
+
 
 def generate_mp3(text, temp_audio='tmp.mp3', accent='en'):
     tts = gTTS(text=text, lang=accent)
@@ -17,7 +17,7 @@ def analyze_audio(text, temp_audio='tmp.mp3', sync_map='word_times.json', temp_t
         of.write(text.replace(' ', '\n'))
               
     ExecuteTaskCLI(use_sys=False).run(arguments=[
-        None, # dummy program name argument
+        None,  # dummy program name argument
         temp_audio,
         temp_text,
         u"task_language=eng|is_text_type=plain|os_task_file_format=json",
@@ -25,7 +25,8 @@ def analyze_audio(text, temp_audio='tmp.mp3', sync_map='word_times.json', temp_t
     os.remove(temp_text)
               
 
-def generate_mp4(sync_map='word_times.json', out_file='myvideo.mp4', temp_audio='tmp.mp3', size=(1200,800), screen_length=20):
+def generate_mp4(sync_map='word_times.json', out_file='myvideo.mp4', temp_audio='tmp.mp3',
+                 size=(1200, 800), screen_length=20):
     clips = []
     text = ''
     line_length = 0
@@ -49,21 +50,17 @@ def generate_mp4(sync_map='word_times.json', out_file='myvideo.mp4', temp_audio=
         current_clip.duration = length
         clips.append(current_clip)
 
-    myaudio = AudioFileClip(temp_audio)
+    my_audio = AudioFileClip(temp_audio)
     cvc = concatenate_videoclips(clips)
-    cvc.audio = myaudio
+    cvc.audio = my_audio
 
     cvc.write_videofile(out_file, fps=25)
     os.remove(temp_audio)
               
-             
-
 
 if __name__ == '__main__':
-    text = "The ABC/Washington Post Poll, even though almost 40% is not bad at this time, was just about the most inaccurate poll around election time!"
-    generate_mp3(text)
-    analyze_audio(text)
+    my_text = "The ABC/Washington Post Poll, even though almost 40% is not bad at this time, was just about the most " \
+           "inaccurate poll around election time! "
+    generate_mp3(my_text)
+    analyze_audio(my_text)
     generate_mp4()
-
-
-    
